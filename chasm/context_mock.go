hanzoai/tasks

PollComponent and PollActivityExecution  (#8563)
## What changed?
- Add `history.ChasmNotifier` for subscribing to CHASM execution state transitions
- Implement `chasm.PollComponent`
- Add `PollActivityExecution` API handler


## Why?
- Needed for standalone activity
- Needed for long-poll of other CHASM archetypes

## How did you test it?
- [x] built
- [x] added new unit test(s)
- [x] added new functional test(s)

chasm/context_mock.go
@@ -8,9 +8,10 @@ import (
 
 // MockContext is a mock implementation of [Context].
 type MockContext struct {
-	HandleExecutionKey func() EntityKey
-	HandleNow          func(component Component) time.Time
-	HandleRef          func(component Component) ([]byte, error)
+	HandleExecutionKey  func() EntityKey
+	HandleNow           func(component Component) time.Time
+	HandleRef           func(component Component) ([]byte, error)
+	HandleStructuredRef func(component Component) (ComponentRef, error)
 }
 
 func (c *MockContext) getContext() context.Context {
@@ -29,6 +30,13 @@ func (c *MockContext) Ref(cmp Component) ([]byte, error) {
 		return c.HandleRef(cmp)
 	}
 	return nil, nil
+}
+
+func (c *MockContext) structuredRef(cmp Component) (ComponentRef, error) {
+	if c.HandleStructuredRef != nil {
+		return c.HandleStructuredRef(cmp)
+	}
+	return ComponentRef{}, nil
 }
 
 func (c *MockContext) ExecutionKey() EntityKey {

dandavison/temporalio-temporal

chasm/context_mock.go
@@ -8,9 +8,10 @@ import (
 
 // MockContext is a mock implementation of [Context].
 type MockContext struct {
-	HandleExecutionKey func() ExecutionKey
-	HandleNow          func(component Component) time.Time
-	HandleRef          func(component Component) ([]byte, error)
+	HandleExecutionKey  func() ExecutionKey
+	HandleNow           func(component Component) time.Time
+	HandleRef           func(component Component) ([]byte, error)
+	HandleStructuredRef func(component Component) (ComponentRef, error)
 }
 
 func (c *MockContext) getContext() context.Context {
@@ -29,6 +30,13 @@ func (c *MockContext) Ref(cmp Component) ([]byte, error) {
 		return c.HandleRef(cmp)
 	}
 	return nil, nil
+}
+
+func (c *MockContext) structuredRef(cmp Component) (ComponentRef, error) {
+	if c.HandleStructuredRef != nil {
+		return c.HandleStructuredRef(cmp)
+	}
+	return ComponentRef{}, nil
 }
 
 func (c *MockContext) ExecutionKey() ExecutionKey {