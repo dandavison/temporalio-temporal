temporalio/temporal

Fix CHASM not found errors

service/history/chasm_engine.go
@@ -776,6 +776,10 @@ func (e *ChasmEngine) getExecutionLease(
 		entityLease.GetReleaseFn()(nil)
 		err = staleReferenceErr
 	}
+	var notFound *serviceerror.NotFound
+	if errors.As(err, &notFound) {
+		err = serviceerror.NewNotFoundf("execution not found")
+	}
 
 	return shardContext, entityLease, err
 }

service/history/chasm_engine_test.go
@@ -545,6 +545,32 @@ func (s *chasmEngineSuite) TestReadComponent_Success() {
 	s.NoError(err)
 }
 
+// TestReadComponent_NotFound tests that ReadComponent does not leak the NotFound message returned
+// by the underlying mutable state read path.
+func (s *chasmEngineSuite) TestReadComponent_NotFound() {
+	s.mockExecutionManager.EXPECT().GetWorkflowExecution(gomock.Any(), gomock.Any()).
+		Return(nil, serviceerror.NewNotFound("this error message will not be returned by ReadComponent")).Times(1)
+
+	err := s.engine.ReadComponent(
+		context.Background(),
+		chasm.NewComponentRef[*testComponent](
+			chasm.EntityKey{
+				NamespaceID: string(tests.NamespaceID),
+				BusinessID:  "non-existent-workflow",
+				EntityID:    "11111111-2222-3333-4444-555555555555",
+			},
+		),
+		func(ctx chasm.Context, component chasm.Component) error {
+			s.Fail("readFn should not be called")
+			return nil
+		},
+	)
+	s.Error(err)
+	var notFound *serviceerror.NotFound
+	s.ErrorAs(err, &notFound)
+	s.Equal("execution not found", notFound.Message)
+}
+
 // TestPollComponent_Success_Wait tests the behavior of PollComponent when the predicate is
 // satisfied at the outset.
 func (s *chasmEngineSuite) TestPollComponent_Success_NoWait() {

dandavison/temporalio-temporal

service/history/chasm_engine.go
@@ -763,6 +763,10 @@ func (e *ChasmEngine) getExecutionLease(
 	if err != nil {
 		return nil, nil, err
 	}
+	var notFound *serviceerror.NotFound
+	if errors.As(err, &notFound) {
+		err = serviceerror.NewNotFound("execution not found")
+	}
 
 	if predicateErr != nil {
 		executionLease.GetReleaseFn()(nil)

service/history/chasm_engine_test.go
@@ -994,6 +994,31 @@ func (s *chasmEngineSuite) TestStateTransitionCount() {
 	s.NoError(err)
 }
 
+// TestReadComponent_NotFound tests that ReadComponent returns an appropriate NotFound error message.
+func (s *chasmEngineSuite) TestReadComponent_NotFound() {
+	s.mockExecutionManager.EXPECT().GetWorkflowExecution(gomock.Any(), gomock.Any()).
+		Return(nil, serviceerror.NewNotFound("this error message will not be returned by ReadComponent")).Times(1)
+
+	err := s.engine.ReadComponent(
+		context.Background(),
+		chasm.NewComponentRef[*testComponent](
+			chasm.ExecutionKey{
+				NamespaceID: string(tests.NamespaceID),
+				BusinessID:  "non-existent-execution",
+				RunID:       "11111111-2222-3333-4444-555555555555",
+			},
+		),
+		func(ctx chasm.Context, component chasm.Component) error {
+			s.Fail("readFn should not be called")
+			return nil
+		},
+	)
+	s.Error(err)
+	var notFound *serviceerror.NotFound
+	s.ErrorAs(err, &notFound)
+	s.Equal("execution not found", notFound.Message)
+}
+
 func (s *chasmEngineSuite) buildPersistenceMutableState(
 	key chasm.ExecutionKey,
 	componentState proto.Message,