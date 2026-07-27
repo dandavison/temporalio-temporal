dc7cf4670ce43781e7d0329f384a32e2f7f26275
Dan Davison <dan.davison@temporal.io>
2026-07-27 16:17:46 -0400

    Increase timer

--- tests/activity_driver.go
+++ tests/activity_driver.go
@@ -132,7 +132,7 @@ func (c activityConfig) modelConfig() model.Config {
 const activityDriverTimeout = 10 * time.Second
 
 // activityDriverTimerMargin is margin added to a timer event's duration when polling for its effect.
-var activityDriverTimerMargin = 2 * timerProcessorMaxShift
+var activityDriverTimerMargin = activityDriverTimeout
 
 // activityDriverPollInterval is the gap between reads when polling for a timer event's effect.
 const activityDriverPollInterval = 100 * time.Millisecond
